4989cad9099a210ccf35c4e176b2d7436b674675
Daniel Lofaro <dan@danlofaro.com>
2013-07-11 14:48:22 -0700

    working on create table

---
+++ itemTableBK.php
@@ -0,0 +1,28 @@
+<table border="0" cellpadding="0" cellspacing="0" width="515">
+<tbody>
+<tr>
+<td align=center> <a href="./"><img src="pix/image001.png" border="0"></a><br>
+<a href="./" class="text">Item Number: JN00052C</a><br>
+<!--<strong class="text">Price:</strong> <span class="text" style="color: rgb(255, 0, 0);">$88</span><br>-->
+<a href="./"><img src="images/but-buy.jpg" border="0"></a><a href="./"><img src="images/but-addtocart.jpg" border="0"></a> </td>
+<td width="28"></td>
+
+
+<td align=center> <a href="./"><img src="pix/image002.png" border="0"></a><br>
+<a href="./" class="text">Item Number: JN00052C</a><br>
+<!--<strong class="text">Price:</strong> <span class="text" style="color: rgb(255, 0, 0);">$88</span><br>-->
+<a href="./"><img src="images/but-buy.jpg" border="0"></a><a href="./"><img src="images/but-addtocart.jpg" border="0"></a> </td>
+<td width="28"></td>
+
+
+<td align=center> <a href="./"><img src="pix/image003.png" border="0"></a><br>
+<a href="./" class="text">Item Number: JN00052C</a><br>
+<!--<strong class="text">Price:</strong> <span class="text" style="color: rgb(255, 0, 0);">$88</span><br>-->
+<a href="./"><img src="images/but-buy.jpg" border="0"></a><a href="./"><img src="images/but-addtocart.jpg" border="0"></a> </td>
+
+
+</tr>
+</tbody>
+</table>
+<a href="./"><img src="images/spacer.gif" border="0" height="17" width="100%"></a>
+
--- main.php
+++ main.php
@@ -1,145 +1,5 @@
 <td align="left" valign="top" width="538"><a href="http://www.templatesfreelance.com"><img src="images/spacer.gif" border="0" height="16" width="100%"></a>
-<table border="0" cellpadding="0" cellspacing="0" width="515">
-<tbody>
-<tr>
-<td align=center> <a href="./"><img src="pix/image001.png" border="0"></a><br>
-<a href="./" class="text">Item Number: JN00052C</a><br>
-<!--<strong class="text">Price:</strong> <span class="text" style="color: rgb(255, 0, 0);">$88</span><br>-->
-<a href="./"><img src="images/but-buy.jpg" border="0"></a><a href="./"><img src="images/but-addtocart.jpg" border="0"></a> </td>
-<td width="28"></td> 
 
-
-<td align=center> <a href="./"><img src="pix/image002.png" border="0"></a><br>
-<a href="./" class="text">Item Number: JN00052C</a><br>
-<!--<strong class="text">Price:</strong> <span class="text" style="color: rgb(255, 0, 0);">$88</span><br>-->
-<a href="./"><img src="images/but-buy.jpg" border="0"></a><a href="./"><img src="images/but-addtocart.jpg" border="0"></a> </td>
-<td width="28"></td> 
-
-
-<td align=center> <a href="./"><img src="pix/image003.png" border="0"></a><br>
-<a href="./" class="text">Item Number: JN00052C</a><br>
-<!--<strong class="text">Price:</strong> <span class="text" style="color: rgb(255, 0, 0);">$88</span><br>-->
-<a href="./"><img src="images/but-buy.jpg" border="0"></a><a href="./"><img src="images/but-addtocart.jpg" border="0"></a> </td>
-
-
-</tr>
-</tbody>
-</table>
-<a href="./"><img src="images/spacer.gif" border="0" height="17" width="100%"></a>
-
-
-
-
-
-
-
-
-
-
-<table border="0" cellpadding="0" cellspacing="0" width="515">
-<tbody>
-<tr>
-<td align=center> <a href="./"><img src="pix/image004.png" border="0"></a><br>
-<a href="./" class="text">Item Number: JN00052C</a><br>
-<!--<strong class="text">Price:</strong> <span class="text" style="color: rgb(255, 0, 0);">$88</span><br>-->
-<a href="./"><img src="images/but-buy.jpg" border="0"></a><a href="./"><img src="images/but-addtocart.jpg" border="0"></a> </td>
-<td width="28"></td> 
-
-
-<td align=center> <a href="./"><img src="pix/image005.png" border="0"></a><br>
-<a href="./" class="text">Item Number: JN00052C</a><br>
-<!--<strong class="text">Price:</strong> <span class="text" style="color: rgb(255, 0, 0);">$88</span><br>-->
-<a href="./"><img src="images/but-buy.jpg" border="0"></a><a href="./"><img src="images/but-addtocart.jpg" border="0"></a> </td>
-<td width="28"></td> 
-
-
-<td align=center> <a href="./"><img src="pix/image006.png" border="0"></a><br>
-<a href="./" class="text">Item Number: JN00052C</a><br>
-<!--<strong class="text">Price:</strong> <span class="text" style="color: rgb(255, 0, 0);">$88</span><br>-->
-<a href="./"><img src="images/but-buy.jpg" border="0"></a><a href="./"><img src="images/but-addtocart.jpg" border="0"></a> </td>
-
-
-</tr>
-</tbody>
-</table>
-<a href="./"><img src="images/spacer.gif" border="0" height="17" width="100%"></a>
-
-
-
-
-
-
-
-
-
-
-
-<table border="0" cellpadding="0" cellspacing="0" width="515">
-<tbody>
-<tr>
-<td align=center> <a href="./"><img src="pix/image007.png" border="0"></a><br>
-<a href="./" class="text">Item Number: JN00052C</a><br>
-<!--<strong class="text">Price:</strong> <span class="text" style="color: rgb(255, 0, 0);">$88</span><br>-->
-<a href="./"><img src="images/but-buy.jpg" border="0"></a><a href="./"><img src="images/but-addtocart.jpg" border="0"></a> </td>
-<td width="28"></td> 
-
-
-<td align=center> <a href="./"><img src="pix/image008.png" border="0"></a><br>
-<a href="./" class="text">Item Number: JN00052C</a><br>
-<!--<strong class="text">Price:</strong> <span class="text" style="color: rgb(255, 0, 0);">$88</span><br>-->
-<a href="./"><img src="images/but-buy.jpg" border="0"></a><a href="./"><img src="images/but-addtocart.jpg" border="0"></a> </td>
-<td width="28"></td> 
-
-
-<td align=center> <a href="./"><img src="pix/image009.png" border="0"></a><br>
-<a href="./" class="text">Item Number: JN00052C</a><br>
-<!--<strong class="text">Price:</strong> <span class="text" style="color: rgb(255, 0, 0);">$88</span><br>-->
-<a href="./"><img src="images/but-buy.jpg" border="0"></a><a href="./"><img src="images/but-addtocart.jpg" border="0"></a> </td>
-
-
-</tr>
-</tbody>
-</table>
-<a href="./"><img src="images/spacer.gif" border="0" height="17" width="100%"></a>
-
-
-
-
-
-
-
-
-
-
-
-
-<table border="0" cellpadding="0" cellspacing="0" width="515">
-<tbody>
-<tr>
-<td align=center> <a href="./"><img src="pix/image010.png" border="0"></a><br>
-<a href="./" class="text">Item Number: JN00052C</a><br>
-<!--<strong class="text">Price:</strong> <span class="text" style="color: rgb(255, 0, 0);">$88</span><br>-->
-<a href="./"><img src="images/but-buy.jpg" border="0"></a><a href="./"><img src="images/but-addtocart.jpg" border="0"></a> </td>
-<td width="28"></td> 
-
-
-<td align=center> <a href="./"><img src="pix/image011.png" border="0"></a><br>
-<a href="./" class="text">Item Number: JN00052C</a><br>
-<!--<strong class="text">Price:</strong> <span class="text" style="color: rgb(255, 0, 0);">$88</span><br>-->
-<a href="./"><img src="images/but-buy.jpg" border="0"></a><a href="./"><img src="images/but-addtocart.jpg" border="0"></a> </td>
-<td width="28"></td> 
-
-
-<td align=center> <a href="./"><img src="pix/image012.png" border="0"></a><br>
-<a href="./" class="text">Item Number: JN00052C</a><br>
-<!--<strong class="text">Price:</strong> <span class="text" style="color: rgb(255, 0, 0);">$88</span><br>-->
-<a href="./"><img src="images/but-buy.jpg" border="0"></a><a href="./"><img src="images/but-addtocart.jpg" border="0"></a> </td>
-
-
-</tr>
-</tbody>
-</table>
-<a href="./"><img src="images/spacer.gif" border="0" height="17" width="100%"></a>
 
 <?php
 
@@ -161,8 +21,54 @@ function createTable($itemTable, $width)
 }
 
 $imgs = $items[2];
-echo createItem($imgs[0],$imgs[1]);
+// echo createItem($imgs[0],$imgs[1]); 
+
+
+echo '
+<table border="0" cellpadding="0" cellspacing="0" width="515">
+<tbody>
+<tr>';
+
+
+$imgs = $items[0];
+echo createItem($imgs[0],$imgs[1]); 
+
+
+
+echo '<td width="28"></td> ';
+
+
+$imgs = $items[1];
+echo createItem($imgs[0],$imgs[1]); 
+
+echo '<td width="28"></td>';
+
+
+$imgs = $items[2];
+echo createItem($imgs[0],$imgs[1]); 
+
+echo '
+</tr>
+</tbody>
+</table>
+<a href="./"><img src="images/spacer.gif" border="0" height="17" width="100%"></a>
+';
+
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 </td>
 </tr>
